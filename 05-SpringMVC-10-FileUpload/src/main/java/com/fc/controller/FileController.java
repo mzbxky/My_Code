ee7482392cc105141a30880357295f4bca3b30dd
mzb <1376055937@qq.com>
2022-04-26 08:54:43 +0800

    文件上传下载

--- 05-SpringMVC-10-FileUpload/src/main/java/com/fc/controller/FileController.java
+++ 05-SpringMVC-10-FileUpload/src/main/java/com/fc/controller/FileController.java
@@ -13,29 +13,90 @@ import java.io.IOException;
 public class FileController {
     @RequestMapping("upload")
     public ModelAndView upload(MultipartFile upload, ModelAndView mv) {
-
-
         //准备路径
         String path = "D:/server/apache-tomcat-8.5.37/webapps/upload";
         File file = new File(path);
-        //如果路径不存在
-        if(!file.exists()){
+        if (!file.exists()){
             file.mkdirs();
         }
         //获取文件名
         String filename = upload.getOriginalFilename();
-
-            //上传
-        try {
-            if(filename!=null) {
-                upload.transferTo(new File(path, filename));
+        if (filename!=null){
+            try {
+                upload.transferTo(new File(path,filename));
                 mv.addObject("img","http://localhost:8081/upload/"+file.getName());
                 mv.setViewName("/success.jsp");
-
-                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+//        //准备路径
+//        String path = "D:/server/apache-tomcat-8.5.37/webapps/upload";
+//        File file = new File(path);
+//        //判断路径是否存在，如果不存在就创建
+//        if (!file.exists()){
+//            file.mkdirs();
+//        }
+//        //获取文件名
+//        String filename = upload.getOriginalFilename();
+//        //如果文件名不为空
+//        if (filename!=null){
+//            try {
+//                upload.transferTo(new File(path,filename));
+//                //将对象添加到模型中
+//                mv.addObject("img","http://localhost:8081/upload/"+file.getName());
+//                mv.setViewName("/success.jsp");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        //准备路径
+//        String path = "D:/server/apache-tomcat-8.5.37/webapps/upload";
+//        File file = new File(path);
+//        //如果路径不存在
+//        //file.exists测试此路径或文件名是否存在
+//        if (!file.exists()){
+//            //创建由抽象路径名表示 的 新目录以及该抽象路径名 的 所有不存在 的 父目录
+//            //如果路径不存在就创建
+//            file.mkdirs();
+//        }
+//        //获取文件名
+//        String filename = upload.getOriginalFilename();
+//        //上传
+//        //如果文件名不为空就上传
+//        if (filename!=null){
+//            try {
+//                upload.transferTo(new File(path,filename));
+//                //将对象添加到模型中
+//                mv.addObject("img","http://localhost:8081/upload/"+file.getName());
+//                //设置跳转路径
+//                mv.setViewName("/success.jsp");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+//        //准备路径
+//        String path = "D:/server/apache-tomcat-8.5.37/webapps/upload";
+//        File file = new File(path);
+//        //如果路径不存在
+//        if(!file.exists()){
+//            file.mkdirs();
+//        }
+//        //获取文件名
+//        String filename = upload.getOriginalFilename();
+//
+//            //上传
+//        try {
+//            if(filename!=null) {
+//                upload.transferTo(new File(path, filename));
+//                mv.addObject("img","http://localhost:8081/upload/"+file.getName());
+//                mv.setViewName("/success.jsp");
+//
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
 
 
 
