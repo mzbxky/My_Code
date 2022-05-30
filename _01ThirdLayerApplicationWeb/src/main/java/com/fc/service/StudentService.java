461a2f1f78862e056413598ebdb381ed2f96e28e
mzb <1376055937@qq.com>
2022-05-30 20:44:52 +0800

    MyBatis的Druid连接池

--- _01ThirdLayerApplicationWeb/src/main/java/com/fc/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.fc.service;
-
-import com.fc.entity.Student;
-import com.fc.vo.PageInfo;
-
-public interface StudentService {
-    //获取PageInfo对象
-    PageInfo<Student> getPageInfo(int pageNo,int pageSize);
-}
